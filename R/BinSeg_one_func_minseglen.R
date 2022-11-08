259453ddff0eda4363fbd6ee3741325396251ade
Rebecca Killick <r.killick@lancaster.ac.uk>
2022-11-08 21:29:42 +0000

    Reverted incorrect changes pulled through rebase

--- R/BinSeg_one_func_minseglen.R
+++ R/BinSeg_one_func_minseglen.R
@@ -1,13 +1,6 @@
-BINSEG = function(sumstat, pen = 0, cost_func = "mean.norm", shape = 1, minseglen = 2,  Q=5){
+BINSEG = function(sumstat, pen = 0, cost_func = "norm.mean", shape = 1, minseglen = 2,  Q=5){
 
   n = length(sumstat[,1]) - 1
-  m = length(sumstat[1,])
-  tol = 0
-  if(cost_func == "mean.norm" || cost_func == "var.norm" || cost_func == "meanvar.norm" || cost_func == "meanvar.exp" || cost_func == "meanvar.gamma" || cost_func == "meanvar.poisson"){
-    MBIC = 0
-  }else{
-    MBIC = 1
-  }
   if(n<2){stop('Data must have at least 2 observations to fit a changepoint model.')}
   if(Q>((n/minseglen)+1)){stop(paste('Q is larger than the maximum number of segments',(n/minseglen)+1))}
   if(Q>n){stop(paste("Q is larger than the length of the data length"))}
@@ -20,12 +13,8 @@ BINSEG = function(sumstat, pen = 0, cost_func = "mean.norm", shape = 1, minsegle
   storage.mode(cptsout)='integer'
   storage.mode(likeout)='double'
   op_cps = 0
-  min = 0
-  optimal = 0
-  max = 0
-  error = 0
 
-  answer=.C('binseg', cost_func = cost_func, sumstat = sumstat, n = as.integer(n), m = as.integer(m), pen = as.double(pen), Q = as.integer(Q), cptsout = cptsout, error = as.integer(error), minorder = as.integer(min), optimalorder = as.integer(optimal), maxorder = as.integer(max), minseglen = as.integer(minseglen), likeout = likeout, op_cps = as.integer(op_cps), shape = as.double(shape), tol = as.double(tol), MBIC = as.integer(MBIC))
+  answer=.C('binseg', cost_func = cost_func, sumstat = sumstat, n = as.integer(n), pen = as.double(pen), Q = as.integer(Q), cptsout = cptsout, minseglen = as.integer(minseglen), likeout = likeout, op_cps = as.integer(op_cps), shape = as.double(shape))
   if((answer$op_cps == Q)&(pen!=0)){warning('The number of changepoints identified is Q, it is advised to increase Q to make sure changepoints have not been missed.')}
   if(answer$op_cps == 0){cpts=n}
   else{cpts=c(sort(answer$cptsout[1:answer$op_cps]),n)}
