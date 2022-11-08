c83f4a59b9b0c6357745d73179c147b11e539d18
Rebecca Killick <r.killick@lancaster.ac.uk>
2022-11-08 15:57:11 +0000

    Update cpt.class.R

--- R/cpt.class.R
+++ R/cpt.class.R
@@ -825,7 +825,7 @@
 		}
 	})
 
-	setMethod("plot","cpt.range",function(x,ncpts=NA,diagnostic=FALSE,cpt.col='red',cpt.width=1,cpt.style=1,type="l"...){
+	setMethod("plot","cpt.range",function(x,ncpts=NA,diagnostic=FALSE,cpt.col='red',cpt.width=1,cpt.style=1,type="l",...){
 	  if(diagnostic==TRUE){
 	    n.changepoints = apply(cpts.full(x), 1, function(x) sum(x > 0, na.rm = TRUE))
 	    penalty.values = pen.value.full(x)
